59d204524e1355bd331999d00420fad1120c11ee
Kavi246 <kaviparmar26@gmail.com>
2022-03-28 13:28:07 +0100

    get/api/topics tested for succesful res

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,8 +10,27 @@ afterAll(() => {
   if (db.end) db.end();
 });
 
-describe.skip('GET/api/topics', () => {
-    test('should return an object', () => {
-        
+describe('GET/api/topics', () => {
+    test('should respond with an array of objects when the request is succesful', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then((response) => {
+            expect(Array.isArray(response.body)).toBe(true);
+        })
     })
+    test('each topic object should have slug property and a description property', () => {
+        return request(app)
+        .get('/api/topics')
+        .expect(200)
+        .then((response) => {
+            response.body.forEach((topic) => {
+                expect(topic).toEqual({
+                    slug: expect.any(String),
+                    description: expect.any(String)
+                })
+            })
+        })
+    })
+
 })
