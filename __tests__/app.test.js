c14fd9300fea0afc8c93c3e6a901b0264b087cab
Kavi246 <kaviparmar26@gmail.com>
2022-03-29 13:44:40 +0100

    setup article patch test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -47,7 +47,7 @@ describe('GET/api/topics', () => {
     })
 })
 
-describe.only('GET/api/articles/:article_id', () => {
+describe('GET/api/articles/:article_id', () => {
     test('should respond with a single matching article when a valid id is requested', () => {
         return request(app)
         .get('/api/articles/1')
@@ -73,12 +73,36 @@ describe.only('GET/api/articles/:article_id', () => {
             expect(res.body.msg).toMatch('Bad request')
         })
     })
-    test.only('status 404: should respond with a article not found message when an id that does not exist is requested', () => {
+    test('status 404: should respond with a article not found message when an id that does not exist is requested', () => {
         return request(app)
         .get('/api/articles/444')
         .expect(404)
         .then((res) => {
             expect(res.body.msg).toEqual('Article not found')
+        })
+    })
+})
+
+describe.only('PATCH /api/articles/:article_id', () => {
+    test('status 200: updates the valid article and responds with that article.', () => {
+        const incrementVotes = { inc_votes: 52}
+        return request(app)
+        .patch('/api/articles/4')
+        .send(incrementVotes)
+        .expect(200)
+        .then((res) => {
+            console.log(res.body)
+            const expected = {
+                article_id: 4,
+                title: 'Student SUES Mitch!',
+                topic: 'mitch',
+                author: 'rogersop',
+                body: 'We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages',
+                created_at: '2020-05-06T01:14:00.000Z',
+                votes: 52
+              }
+            expect(res.body.updatedArticle).toEqual(expected)
+
         })
     })
 })
