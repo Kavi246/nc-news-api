d03e2f959f8a9935c68b1f41cf8e15affd1c0802
Kavi246 <kaviparmar26@gmail.com>
2022-03-29 12:33:21 +0100

    spaced out

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const {getAllTopics} = require('./controllers/topics')
-const {getArticleById} = require('./controllers/articles')
+const { getAllTopics } = require('./controllers/topics')
+const { getArticleById } = require('./controllers/articles')
 
 const app = express();
 app.use(express.json());
--- controllers/articles.js
+++ controllers/articles.js
@@ -1,7 +1,7 @@
 const { selectArticleById } = require('../models/articles');
 
 exports.getArticleById = (req, res, next) => {
-    const {article_id} = req.params;
+    const { article_id } = req.params;
     selectArticleById(article_id).then((article) => {
         res.status(200).send({article})
     })
