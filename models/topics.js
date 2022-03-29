b73b5bf574df1ad7ba251431cc4c067865083324
Kavi246 <kaviparmar26@gmail.com>
2022-03-29 12:07:57 +0100

    semicolon syntax

--- models/topics.js
+++ models/topics.js
@@ -1,6 +1,6 @@
 const db = require('../db/connection')
 
 exports.selectAllTopics = async () => {
-    const allTopics = await db.query('SELECT * FROM topics');
+    const allTopics = await db.query('SELECT * FROM topics;');
     return allTopics.rows;
 }
