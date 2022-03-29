f0737534b487babb0960bd58c99b87b4629c677f
Kavi246 <kaviparmar26@gmail.com>
2022-03-29 16:36:21 +0100

    removed comment

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -14,7 +14,6 @@ exports.patchArticleById = (req, res, next) => {
 
     if(!req.body.hasOwnProperty('inc_votes')) {
         return next({status: 400, msg: 'patch request body incorrectly formatted'})
-        //return Promise.reject({status: 400, msg: 'patch request body incorrectly formatted'})
     }
     else if( typeof req.body.inc_votes !== 'number') {
         return next({status: 400, msg: 'value for the vote increment must be a number!'})
