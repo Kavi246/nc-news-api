99d2dfcbb226f34a01ac040cd9ceb349fcfb2cd1
Kavi246 <kaviparmar26@gmail.com>
2022-03-31 13:57:40 +0100

    refactored to one sql query

--- models/articles.models.js
+++ models/articles.models.js
@@ -1,15 +1,17 @@
 const db = require('../db/connection')
 
 exports.selectArticleById = async (article_id) => {
-    const article = await db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id]);
+    const article = await db.query(`
+    SELECT articles.*, CAST(COUNT(comments.comment_id) AS INT) AS comment_count
+    FROM articles LEFT JOIN comments 
+        ON comments.article_id = articles.article_id 
+    WHERE articles.article_id = $1 
+    GROUP BY articles.article_id;`
+    , [article_id]);
     if(article.rows.length === 0) {
         return Promise.reject({status: 404, msg: "Article not found"})
     }
-    const chosenArticle = article.rows[0]
-
-    const commentsByArticleId = await db.query('SELECT * FROM comments WHERE article_id = $1;', [article_id]);
-    chosenArticle['comment_count'] = commentsByArticleId.rows.length;
-    return chosenArticle;
+    return article.rows[0]
 }
 
 exports.updateArticleById = async (article_id, inc_votes) => {
