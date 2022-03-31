7171989284624e135ec4cec9a6c9a11d4b7f179b
Kavi246 <kaviparmar26@gmail.com>
2022-03-31 11:34:17 +0100

    view tables in txtfile

--- db/viewTables.sql
+++ db/viewTables.sql
@@ -1,3 +1,17 @@
 \c nc_news_test;
 
+\echo 'Articles'
+
 SELECT * FROM articles;
+
+\echo 'Topics'
+
+SELECT * FROM topics;
+
+\echo 'Users'
+
+SELECT * FROM users;
+
+\echo 'Comments'
+
+SELECT * FROM comments;
