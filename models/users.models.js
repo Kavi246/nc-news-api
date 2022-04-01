cec481fde2a62643ebf142598dde2e74eeeff6c0
Kavi246 <kaviparmar26@gmail.com>
2022-04-01 17:07:22 +0100

    get comments of an article done

--- models/users.models.js
+++ models/users.models.js
@@ -3,7 +3,6 @@ const db = require('../db/connection')
 exports.selectAllUsers = () => {
     return db.query(`SELECT username FROM users;`)
     .then((result) => {
-        console.log(result.rows)
         return result.rows
     })
 }
