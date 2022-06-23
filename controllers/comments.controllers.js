126e7b91de88797868b5f23ddba3ca42e90fa660
Kavi246 <kaviparmar26@gmail.com>
2022-06-23 10:39:42 +0100

    delete comments responds with no content

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -3,8 +3,7 @@ const { deleteComment } = require('../models/comments.models');
 exports.removeComment = (req, res, next) => {
    const { comment_id } = req.params;
     deleteComment(comment_id).then((result) => {
-        console.log(result.msg)
-        res.status(result.status).send(result.msg);
+        res.sendStatus(result.status);
     })
 
     
